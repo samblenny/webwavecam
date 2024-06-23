bcda8087710d5d0291e234c41bb07f2013bbf4ee
sam blenny <68084116+samblenny@users.noreply.github.com>
2024-06-23 08:04:15 +0000

    Add video stream start/stop controls

--- main.js
+++ main.js
@@ -2,4 +2,72 @@
 /* SPDX-FileCopyrightText: Copyright 2024 Sam Blenny */
 "use strict";
 
-console.log("TODO: make stuff");
+const STATUS = document.querySelector('#status');   // Status span
+const CAM_BTN = document.querySelector('#camera');  // Camera button
+const VIDEO = document.querySelector('#video');     // Video player
+
+// Camera video stream
+var STREAM = null;
+
+// Update status line span
+function setStatus(s) {
+    STATUS.textContent = s;
+}
+
+// Attempt to open video stream from default camera
+function startVideo() {
+    const constraints = {video: {width: 640, height: 480}}
+    navigator.mediaDevices.getUserMedia(constraints)
+    .then((stream_) => {
+        CAM_BTN.classList.add('on');  // Update UI button
+        CAM_BTN.textContent = 'pause';
+        STREAM = stream_;
+        navigator.mediaDevices.ondevicechange = deviceChange;
+        VIDEO.srcObject = stream_;    // Start video preview
+        VIDEO.play();
+        let cameras = [];
+        for(let t of stream_.getTracks()) {
+            cameras.push(t.label);
+        }
+        setStatus(cameras.join(", "));
+    })
+    .catch((err) => {
+        setStatus("failed to start camera");
+        console.log("unable to open video stream", err);
+        alert("I wasn't able to open any cameras. Maybe your privacy settings "
+            + "don't allow camera access, or perhaps a cable is unplugged.");
+    });
+}
+
+// Handle possibility of USB webcam being suddenly unplugged
+function deviceChange(d) {
+    if(STREAM && !STREAM.active) {
+        pauseVideo();
+        setStatus("paused (lost video stream)");
+    }
+}
+
+// Pause playback at current frame, then let go of camera's video stream
+function pauseVideo() {
+    VIDEO.pause();    // freeze video playback at current frame
+    CAM_BTN.classList.remove('on');
+    CAM_BTN.textContent = 'Start Camera';
+    for(let t of STREAM.getTracks()) {
+        t.stop();     // turn off camera (LED should go off)
+    }
+    STREAM = null;
+    setStatus("paused");
+}
+
+// Add camera on/off event handlers to the camera button
+CAM_BTN.addEventListener('click', function() {
+    if(CAM_BTN.classList.contains('on')) {
+        // Camera was on, so turn it off
+        pauseVideo();
+    } else {
+        // Camera was off, so attempt to turn it on
+        startVideo();
+    }
+});
+
+setStatus("ready");
